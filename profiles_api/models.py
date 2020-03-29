a77b3f27ecf1df26e4620016039b4d6a8e147aab
suadolaps <suadkamardeen@gmail.com>
2020-03-29 16:00:10 +0200

    Create profile serializer

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,7 +13,7 @@ class UserProfileManager(BaseUserManager):
 		email = self.normalize_email(email)
 		user = self.model(email=email, name=name)
 
-		user.set_password(password)
+		user.set_password(password) # hashes the password
 		user.save(using=self._db)
 
 		return user
